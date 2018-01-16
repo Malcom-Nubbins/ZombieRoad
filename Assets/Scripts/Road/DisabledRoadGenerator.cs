d7edcfec80bb6f029181b1f2dbb43285c900ad40
rusty595 <rusty595@users.noreply.github.com>
2018-01-16 16:08:18 +0000

    Fixed the edge of the world being within the cull boundary

--- Assets/Scripts/Road/DisabledRoadGenerator.cs
+++ Assets/Scripts/Road/DisabledRoadGenerator.cs
@@ -32,20 +32,25 @@ public class DisabledRoadGenerator : RoadGenerator
 					int k = 1;
 					Vector3 RayLoc = transform.position + new Vector3(k * Xoffset(j) + Xoffset(i), 500, k * Zoffset(j) + Zoffset(i));
 					Vector3 RayLocNoY = new Vector3(RayLoc.x, 0, RayLoc.z);
-					while (!hitPlus[j].collider && (Vector3.Distance(CachedPlayerPosition, RayLocNoY)/*Vector3.Distance(RoadTileManager.checkpoint.transform.position, RayLocNoY)*/ < /*(RoadTileManager.checkpoint.gameObject.transform.localScale.x / 2) + 20*/(RoadTileManager.checkpoint.FollowCamera.GetComponent<FollowCamera>().CullDistance + 100)))
+					while (!hitPlus[j].collider && (Vector3.Distance(CachedPlayerPosition, RayLocNoY) < (RoadTileManager.checkpoint.FollowCamera.GetComponent<FollowCamera>().CullDistance + 100)))
 					{
-						//Debug.Log(gameObject.name + gameObject.transform.position + " \n Distance checkpoint->ray: " + Vector3.Distance(RoadTileManager.checkpoint.transform.position, RayLocNoY) + " < " + ((RoadTileManager.checkpoint.gameObject.transform.localScale.x / 2) + 20) + " \n Distance player->ray: " + Vector3.Distance(CachedPlayerPosition, RayLocNoY) + " < " + (RoadTileManager.checkpoint.FollowCamera.GetComponent<FollowCamera>().CullDistance + 20));
-						Physics.Raycast(RayLoc, new Vector3(0, -1), out hitPlus[j], Mathf.Infinity, LayerMask.GetMask("Road")/*1 << 9*/);
-						//MySpecificDebug += "Considering " + (Direction)i + ", scanning " + (Direction)j + " hit " + (hitPlus[j].collider ?  hitPlus[j].collider.name + hitPlus[j].collider.transform.position : "nothing "+RayLocNoY) + "\n";
+						Physics.Raycast(RayLoc, new Vector3(0, -1), out hitPlus[j], Mathf.Infinity, LayerMask.GetMask("Road"));
 						k++;
 						RayLoc = transform.position + new Vector3(k * Xoffset(j) + Xoffset(i), 500, k * Zoffset(j) + Zoffset(i));
 						RayLocNoY.x = RayLoc.x;
 						RayLocNoY.z = RayLoc.z;
 					}
 					if (hitPlus[j].collider && hitPlus[j].collider.GetComponent<RoadGenerator>().Exit.Length < 8) hitPlus[j].collider.GetComponent<RoadGenerator>().RefreshExits();
-					if (!hitPlus[j].collider ||/* hitPlus[j].collider &&*/ !hitPlus[j].collider.GetComponent<DisabledRoadGenerator>())
+
+					MySpecificDebug += (RoadTileManager.checkpoint.FollowCamera.GetComponent<FollowCamera>().CullDistance) - Vector3.Distance(CachedPlayerPosition, transform.position) + "\n";
+
+					//if (!((RoadTileManager.checkpoint.FollowCamera.GetComponent<FollowCamera>().CullDistance) - Vector3.Distance(CachedPlayerPosition, transform.position) < 20 && (Vector3.Distance(CachedPlayerPosition, RayLocNoY) > (RoadTileManager.checkpoint.FollowCamera.GetComponent<FollowCamera>().CullDistance + 100))))
+					//{
+					//	MySpecificDebug += "Nearby boundary hit while hole scanning\n";
+					//	bProbablyHole = true;
+					//}
+					if ((!((RoadTileManager.checkpoint.FollowCamera.GetComponent<FollowCamera>().CullDistance) - Vector3.Distance(CachedPlayerPosition, transform.position) < 20 && (Vector3.Distance(CachedPlayerPosition, RayLocNoY) > (RoadTileManager.checkpoint.FollowCamera.GetComponent<FollowCamera>().CullDistance + 100)))) && (!hitPlus[j].collider || !hitPlus[j].collider.GetComponent<DisabledRoadGenerator>()))
 					{
-						//MySpecificDebug += "Considering " + (Direction)i + ", scanning " + (Direction)j + (hitPlus[j].collider ? " hit " + hitPlus[j].collider.name + hitPlus[j].collider.transform.position : " reached oob") + ", PROBABLY NOT HOLE" + "\n";
 						bProbablyHole = false;
 					}
 				}
