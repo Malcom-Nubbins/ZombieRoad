93d742026cd8ba1c19911f1483b64d51147e8b3f
BCrawford <bencrawford101@gmail.com>
2018-01-30 16:07:08 +0000

    Added player walk animation

---
+++ Assets/Scripts/Player/PlayPlayerWalkingAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayPlayerWalkingAnimation : MonoBehaviour
+{
+    Animator animator;
+
+	void Start()
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update()
+    {
+        bool moving = Movement.InputLeft() || Movement.InputRight();
+        animator.SetBool("Walking", moving);
+	}
+}
