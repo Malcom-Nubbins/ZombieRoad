c2c47db2881b14a539413b6a4a5f01a7844ddf08
rusty595 <rusty595@users.noreply.github.com>
2018-02-06 15:01:33 +0000

    Zombies on the Main Menu now meander aimlessly between the centre of the city and the camera

--- Assets/Scripts/Zombies/SeekPlayer.cs
+++ Assets/Scripts/Zombies/SeekPlayer.cs
@@ -12,6 +12,9 @@ public class SeekPlayer : MonoBehaviour
     public float wanderCircleRotationSpeed = 90.0f;
     float wanderCircleCurrentAngle = 0.0f;
     Rigidbody rb;
+	public Vector3 MainMenuDest;
+	Vector3 seekDirection;
+	float AIdelay;
 
 	void Start()
     {
@@ -23,39 +26,45 @@ public class SeekPlayer : MonoBehaviour
         GetComponent<Health>().onDeath += () => {
             enabled = false;
         };
+		MainMenuDest = new Vector3(Random.value * 200, 0, Random.value * -200) + RoadTileManager.checkpoint.RoadMapRoot.transform.position;
+		AIdelay = Random.value;
 	}
 	
 	void Update()
     {
-        Vector3 toPlayer = (RoadTileManager.bMainMenu ? followCamera.MainMenuZombieTarget.transform.position : followCamera.target.transform.position) - transform.position;
+		Vector3 toPlayer = (RoadTileManager.bMainMenu ? MainMenuDest : followCamera.target.transform.position) - transform.position;
         toPlayer.y = 0;//dont move up or down, physics will handle that
 
-        if (toPlayer.magnitude <= 200)
+        if (RoadTileManager.bMainMenu || toPlayer.magnitude <= 200)
         {
-            Vector3 seekDirection = toPlayer.normalized;
-            float seekAngle = Mathf.Atan2(seekDirection.z, seekDirection.x);
-            //Debug.DrawRay(transform.position + transform.up, seekDirection * 5, Color.green);
+			seekDirection = toPlayer.normalized;
+			float seekAngle = Mathf.Atan2(seekDirection.z, seekDirection.x);
+			//Debug.DrawRay(transform.position + transform.up, seekDirection * 5, Color.green);
 
-            wanderCircleCurrentAngle += Random.Range(-1.0f, 1.0f) * wanderCircleRotationSpeed * Time.deltaTime;
-            if (wanderCircleCurrentAngle < 0) wanderCircleCurrentAngle += 360.0f;
-            if (wanderCircleCurrentAngle >= 360.0f) wanderCircleCurrentAngle -= 360.0f;
-            //add seekAngle so if wanderCircleCurrentAngle is 0 it will move in the seek direction instead of wandering towards positive x
-            Vector3 wanderCircleDirection = new Vector3(
-                Mathf.Cos(wanderCircleCurrentAngle * Mathf.Deg2Rad + seekAngle),
-                0,
-                Mathf.Sin(wanderCircleCurrentAngle * Mathf.Deg2Rad + seekAngle)
-            );
-            Vector3 pointOnWanderCirclePosition = (seekDirection * wanderCircleDistanceInFront) + wanderCircleDirection * wanderCircleRadius;
-            Vector3 pointOnWanderCircleDirection = pointOnWanderCirclePosition.normalized;
-            //Debug.DrawRay(transform.position + transform.up, pointOnWanderCircleDirection * 5, Color.red);
+			wanderCircleCurrentAngle += Random.Range(-1.0f, 1.0f) * wanderCircleRotationSpeed * Time.deltaTime;
+			if (wanderCircleCurrentAngle < 0) wanderCircleCurrentAngle += 360.0f;
+			if (wanderCircleCurrentAngle >= 360.0f) wanderCircleCurrentAngle -= 360.0f;
+			//add seekAngle so if wanderCircleCurrentAngle is 0 it will move in the seek direction instead of wandering towards positive x
+			Vector3 wanderCircleDirection = new Vector3(
+				Mathf.Cos(wanderCircleCurrentAngle * Mathf.Deg2Rad + seekAngle),
+				0,
+				Mathf.Sin(wanderCircleCurrentAngle * Mathf.Deg2Rad + seekAngle)
+			);
+			Vector3 pointOnWanderCirclePosition = (seekDirection * wanderCircleDistanceInFront) + wanderCircleDirection * wanderCircleRadius;
+			Vector3 pointOnWanderCircleDirection = pointOnWanderCirclePosition.normalized;
+			//Debug.DrawRay(transform.position + transform.up, pointOnWanderCircleDirection * 5, Color.red);
 
-            Vector3 moveDirection = pointOnWanderCircleDirection;
+			Vector3 moveDirection = pointOnWanderCircleDirection;
 
-            Quaternion rotation = transform.rotation;
-            rotation.SetLookRotation(moveDirection, transform.up);
-            transform.rotation = rotation;
+			Quaternion rotation = transform.rotation;
+			rotation.SetLookRotation(moveDirection, transform.up);
+			transform.rotation = rotation;
 
             transform.position = transform.position + transform.forward * speed * Time.deltaTime;
         }
+		if (RoadTileManager.bMainMenu)
+		{
+			if (toPlayer.magnitude <= 20) MainMenuDest = new Vector3(Random.value * 200, 0, Random.value * -200) + RoadTileManager.checkpoint.RoadMapRoot.transform.position;
+		}
 	}
 }
