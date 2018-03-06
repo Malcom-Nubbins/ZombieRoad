7c7e2c5ccaaf1469f575903c0f8ff247c70444cc
Bartosz Grabowski <badylaz@gmail.com>
2018-03-06 13:48:48 +0000

    Added main menu music persisting between different scenes

---
+++ Assets/Scripts/mainMenuMusic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuMusic : MonoBehaviour
+{
+
+    AudioSource musicSource;
+    GameObject musicBox;
+    // Use this for initialization
+    void OnEnable()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("MusicBox");
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        musicSource = this.gameObject.GetComponent<AudioSource>();
+       // musicSource.Play();
+        musicBox = this.gameObject;
+
+        DontDestroyOnLoad(musicBox);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(SceneManager.GetActiveScene().name =="TestScene")
+        {
+            musicSource.Stop();
+        }
+        else
+        {
+            if(!musicSource.isPlaying)
+            {
+                musicSource.Play();
+            }
+        }
+    }
+}
