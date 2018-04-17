feb677cbee4869dbcf537f2470a52608d554fc1e
rusty595 <rusty595@users.noreply.github.com>
2018-04-17 11:13:53 +0100

    Rebalanced vehicle spawn rate

--- Assets/ExtraVehicleSpawner.cs
+++ Assets/ExtraVehicleSpawner.cs
@@ -11,7 +11,7 @@ public class ExtraVehicleSpawner : MonoBehaviour
         TilePosition pos = new TilePosition(transform.position);
         foreach (TilePosition neighborOffset in new TilePosition[] { new TilePosition(-1, 0), new TilePosition(1, 0), new TilePosition(0, -1), new TilePosition(0, 1) })
         {
-            if (Random.value < spawnChance)
+            if (Random.value < gameObject.GetComponent<Unlockable>().Price / 100.0f)
             {
                 WorldTile tile = WorldTileManager.instance.GetTile(pos + neighborOffset);
                 if (tile)
