94517a2a4b959659ce3c1572805b530760aaed63
rusty595 <rusty595@users.noreply.github.com>
2020-06-10 22:33:11 +0100

    Update MapSelection.cs

--- Assets/Scripts/MapSelection.cs
+++ Assets/Scripts/MapSelection.cs
@@ -5,7 +5,7 @@ public class MapSelection : MonoBehaviour
 {
 	[SerializeField, NonEmpty] string selectedMap;
 	public string GetSelectedMap() { return selectedMap; }
-	public bool SetSelectedMap(string NewSelection) { if (SceneManager.GetSceneByName(NewSelection).IsValid()) { selectedMap = NewSelection; return true; } return false; }
+	public bool SetSelectedMap(string NewSelection) { if (Application.CanStreamedLevelBeLoaded(NewSelection)) { selectedMap = NewSelection; return true; } return false; }
 
 	public string[] AvailableMapNames;
 	public Texture2D[] AvailableMapImages;
