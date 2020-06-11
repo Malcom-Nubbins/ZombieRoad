f7ce0d88573d8a2fe3de3ce8c8167383e2da2e86
rusty595 <rusty595@users.noreply.github.com>
2020-06-11 22:35:15 +0100

    really rename MainMenuMusic

---
+++ Assets/Scripts/MainMenuMusic.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuMusic : MonoBehaviour
+{
+	static MainMenuMusic inst;
+
+	[SerializeField, NonNull] AudioSource musicSource;
+	
+	void OnEnable()
+	{
+		if (inst != null && inst != this)
+		{
+			Destroy(this);
+		}
+
+		// musicSource.Play();
+		AudioListener.volume = PlayerPrefs.GetFloat("MasterVolume");
+
+		DontDestroyOnLoad(gameObject);
+		inst = this;
+	}
+	
+	void Update()
+	{
+		if (SceneManager.GetActiveScene().name == "Sbuburbs")
+		{
+			musicSource.Stop();
+		}
+		else
+		{
+			if (!musicSource.isPlaying)
+			{
+				musicSource.Play();
+			}
+		}
+	}
+}
